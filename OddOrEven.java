8dc30cfad5312a3b50c5fda05c4cfed98d65cc24
Nisha-Velmurugan <115609486+Nisha-Velmurugan@users.noreply.github.com>
2025-02-11 01:36:41 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OddEvenChecker {
+public class OddOrEven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
