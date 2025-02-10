f794dc26c8c69f774989fb0be2b0f969fdaeaaf0
Nisha-Velmurugan <115609486+Nisha-Velmurugan@users.noreply.github.com>
2025-02-11 01:37:49 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
-
 public class OddOrEven {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        if (number % 2 == 0) {
-            System.out.println(number + " is Even.");
-        } else {
-            System.out.println(number + " is Odd.");
+        if (args.length == 0) {
+            System.out.println("Please provide a number as a command-line argument.");
+            return;
         }
 
-        scanner.close();
+        try {
+            int number = Integer.parseInt(args[0]);
+
+            if (number % 2 == 0) {
+                System.out.println(number + " is Even.");
+            } else {
+                System.out.println(number + " is Odd.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input! Please enter a valid integer.");
+        }
     }
 }
-
